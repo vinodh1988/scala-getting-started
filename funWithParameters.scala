6beda3dc9ebdf2d9957e805a950421bb8fcf8657
vinodh1988 <admin@Vinodh.local>
2024-05-14 16:58:42 +0530

    block

--- funWithParameters.scala
+++ funWithParameters.scala
@@ -9,4 +9,10 @@ def process(a: String): Unit = {
 object mainapp extends App {
 println(fun1(20))
 process("Roger")
+val blockResult = {
+    val a=fun1(20)
+    val b=fun1(30)
+    a+b
+} //block assigned to a variable
+println(s"Block result is $blockResult")
 }
