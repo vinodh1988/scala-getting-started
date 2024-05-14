a6951f079d496186a4b60d96687f576db48c7122
vinodh1988 <admin@Vinodh.local>
2024-05-14 16:11:01 +0530

    commandlineparameters

---
+++ Command.scala
@@ -0,0 +1,6 @@
+@main def runIt(process: String, name: String)= {
+    println(s"Process :$process name : $name")
+}
+
+//command line parameters
+//while running scala command.scala  processname name
--- Third.scala
+++ Third.scala
@@ -3,3 +3,5 @@ object TryThis {
        print("Java Style Main")
     }
 }
+
+//https://github.com/vinodh1988/scala-getting-started
