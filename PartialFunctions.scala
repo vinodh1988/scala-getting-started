769e6b75ea189c97aaf1521d95995ed58f65a53d
vinodh1988 <admin@Vinodh.local>
2024-05-15 12:36:57 +0530

    reciprocal-partialfunction

---
+++ PartialFunctions.scala
@@ -0,0 +1,7 @@
+val reciprocal: PartialFunction[Double, Double] = {
+  case x if x != 0.0 => 1.0 / x
+}
+@main def doThis() = {
+println(reciprocal.isDefinedAt(2.0)) // true
+println(reciprocal.isDefinedAt(0.0)) // false
+}
