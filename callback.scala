2564bf449dcb77aed207b7537c722261714dd903
vinodh1988 <admin@Vinodh.local>
2024-05-15 11:45:10 +0530

    callback

---
+++ callback.scala
@@ -0,0 +1,28 @@
+def bigTaskLibrary(data: String, callback: (String)=> String): Unit= {
+    println("Task Started")
+    println(s"Processing $data")
+    val userp=callback(s"Process the process $data")
+    println(s"Process the $userp that is given by the user")
+    val finaldata=callback("Finish")
+    if(finaldata=="success")
+       println("Wrapping the task")
+    else
+       println("it seems the user logic has not been done properly")
+} 
+
+//User1
+def userActivity(info: String): String = {
+  if(info == "Finish")
+      {
+        println("Finishing the user function")
+        return "success"
+      }
+  else {
+       println("user logic is running")
+       return s"[User Processed $info]"
+  }
+}
+@main def runTheBlock = {
+//Calling the library
+bigTaskLibrary("Callback Scala Task",userActivity)
+}
