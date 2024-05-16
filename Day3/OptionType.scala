53dd66e73e47290c0b85331448a63e4a6d6977bf
vinodh1988 <admin@Vinodh.local>
2024-05-16 10:48:56 +0530

    optional-continued

--- Day3/OptionType.scala
+++ Day3/OptionType.scala
@@ -1,9 +1,16 @@
 //optionalType
 val optionalInt: Option[Int] = Some(42)
+val second: Option[Int] = None
 //usual type
 val normal: Int =45
 
 val actualValue:Int= optionalInt match {
   case Some(value) => value
   case None => -1
+}
+
+object Runner extends App {
+    println(actualValue)
+    println(optionalInt.getOrElse(-1))
+    println(second.getOrElse(-1))
 }
