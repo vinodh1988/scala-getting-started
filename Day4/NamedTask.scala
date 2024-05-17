792a8cdf3bd7fbd9ec5885bb0b73f84ffde9e3d5
vinodh1988 <admin@Vinodh.local>
2024-05-17 10:18:45 +0530

    block-application-with-other-parameters

--- Day4/NamedTask.scala
+++ Day4/NamedTask.scala
@@ -1,20 +1,20 @@
-class Task {
+class Task(name: String) {
   def doSomething(): Unit = {
     // method implementation
-    println("Do it good")
+    println(s"Do $name in a good way")
   }
 }
 
 object Task {
-  def apply(block: => Unit): Task = {
-    val task = new Task()
+  def apply(name: String)(block: => Unit): Task = {
+    val task = new Task(name)
     block // Execute the block
     task
   }
 }
 
 @main def MainRunner(): Unit = {
-val myTask = Task{
+val myTask = Task("Coding Task") {
   println("This code block is executed immediately inside the apply method.")
 }
 myTask.doSomething()
