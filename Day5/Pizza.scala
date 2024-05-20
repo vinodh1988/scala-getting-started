59d0f8a669361c1f57c657a33cec39b1bb2bb3d9
vinodh1988 <admin@Vinodh.local>
2024-05-20 10:06:35 +0530

    one-more-trait

---
+++ Day5/Pizza.scala
@@ -0,0 +1,40 @@
+trait Pizza
+{
+    def prepare: Unit
+}
+trait Saucing extends Pizza {
+    abstract override def prepare: Unit = {
+        super.prepare
+        println("Apply As much Sauce as you can")
+    }
+}
+
+trait OlivesTopping extends Saucing {
+    abstract override def prepare: Unit = 
+        {   
+            super.prepare
+            println("Adding Tomato, Onion and Olives Topping")}
+}
+
+trait MushrooomTopping extends Saucing {
+    abstract override def prepare: Unit = 
+        {   
+            super.prepare
+            println("Adding Tomato, Onion and Mushroom Topping")
+        }
+}
+
+class PizzaMaking extends Pizza
+{
+    override def prepare: Unit = {
+
+        println("Baking pizza base")
+    }
+}
+
+object MainIt extends App {
+    val pizzanow: PizzaMaking = new PizzaMaking with Saucing with MushrooomTopping
+    pizzanow.prepare
+    val pizzanow2: PizzaMaking = new PizzaMaking with Saucing with OlivesTopping
+    pizzanow2.prepare
+}
